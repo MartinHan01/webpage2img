15d28d0c99009a04f60c1a413bf3b5b7ece629e4
zoey <1550869559@qq.com>
2018-07-07 01:11:53 +0800

    update script

--- main.py
+++ main.py
@@ -11,17 +11,16 @@ import os
 import pictureutil
 
 
-filelist = [
-    'https://mp.weixin.qq.com/s/zL0RLdc9TQdR9-tjkMM_PA'
-    
-]
+filelist = []
 
 def save_url(driver, url, dir):
     index = 0
     driver.get(url)
     title_element = driver.find_element_by_xpath("//div//h2[@class='rich_media_title']")
+    if title_element == None:
+        return 
     title = title_element.text
-    pic_path = os.path.join(dir, 'result', title)
+    pic_path = os.path.join(dir, title)
     fileutil.mkdirs_if_not_exist(pic_path)
 
     
@@ -37,33 +36,42 @@ def save_url(driver, url, dir):
         height = screen_height * (i + 1)
         js = "window.scrollTo(0,%d)" % (height)
         driver.execute_script(js)
+    return os.path.abspath(pic_path),title
     
     
-def package_picture(dir, save_path):
-    if os.chdir(dir) == False:
-        print('chdir ' + str(dir) + ' error')
-        exit(-1)
-    if os.path.isdir(dir) == False:
-        print(str(dir) + ' is not a dir')
-        exit(-1)
+def package_picture(dir, save_path, pic_name):
+    
     
     list = os.listdir(dir)
-    for i in list:
-        title = i
-        pic_dir = os.path.join(dir, title)
-        pic_list = os.listdir(pic_dir)
-        save_file = os.path.join(save_path, title + '.png')
-        pictureutil.merge_pic(pic_dir, pic_list, os.path.abspath(save_file))
+    list.sort()
+    pic_dir = dir
+    pic_list = os.listdir(pic_dir)
+    pic_list.sort()
+    save_file = os.path.join(save_path, pic_name + '.png')
+    pictureutil.merge_pic(pic_dir, pic_list, os.path.abspath(save_file))
 
-
+def init_filelist():
+    f = open('./test.txt', 'r')
+    for line in open('./test.txt'):
+        line = f.readline()
+        if line == '' or line == '\n':
+            continue
+        filelist.append(line)
 
 
 if __name__ == '__main__':
+    init_filelist()
     driver = webdriver.Chrome()
     dir = './result'
-    for item in filelist:
-        save_url(driver, item, dir)
     
-    package_picture(dir, os.path.join(dir))
+    for item in filelist:
+        try:
+            pic_path,title = save_url(driver, item, dir)
+            package_picture(pic_path, os.path.abspath(dir), title)
+        except Exception as e :
+            print(e)
+            
+    
+           
 
     
